0fb9b3692ff9501976981a2799879364430a4aa7
khelifi waleed <ton-email@gmail.com>
2025-05-18 02:50:04 +0100

    Design Patterns Builder

---
+++ src/Builder/CarBuilder.php
@@ -0,0 +1,35 @@
+<?php
+namespace DesignPatterns\Builder;
+
+class CarBuilder
+{
+    private CarInterface $car;
+
+    public function __construct(CarInterface $car)
+    {
+        $this->car = $car;
+    }
+
+    public function setEngine(string $engine): self
+    {
+        $this->car->setEngine($engine);
+        return $this;
+    }
+
+    public function setColor(string $color): self
+    {
+        $this->car->setColor($color);
+        return $this;
+    }
+
+    public function setDoors(int $doors): self
+    {
+        $this->car->setDoors($doors);
+        return $this;
+    }
+
+    public function build(): CarInterface
+    {
+        return $this->car;
+    }
+}
---
+++ src/Builder/FamilyCar.php
@@ -0,0 +1,29 @@
+<?php
+namespace DesignPatterns\Builder;
+
+class FamilyCar implements CarInterface
+{
+    private string $engine;
+    private string $color;
+    private int $doors;
+
+    public function setEngine(string $engine): void
+    {
+        $this->engine = $engine;
+    }
+
+    public function setColor(string $color): void
+    {
+        $this->color = $color;
+    }
+
+    public function setDoors(int $doors): void
+    {
+        $this->doors = $doors;
+    }
+
+    public function show(): void
+    {
+        echo "Family Car - Engine: {$this->engine}, Color: {$this->color}, Doors: {$this->doors}\n";
+    }
+}
